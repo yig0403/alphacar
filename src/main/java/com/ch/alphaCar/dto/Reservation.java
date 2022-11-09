5625a956cda56d0e1df6e8c4954cc21f1e03524b
KibeomPark <pkb0415@gmail.com>
2022-11-09 17:43:47 +0900

    11/09 기범 마무리(한게없다...)

--- src/main/java/com/ch/alphaCar/dto/Reservation.java
+++ src/main/java/com/ch/alphaCar/dto/Reservation.java
@@ -8,8 +8,11 @@ public class Reservation {
  private int rsNo;                //예약번호
  private Date regDate;            //예약날짜
  
- private String startDate;        //렌트날짜 
- private String endDate;          //반납날짜
+ private int startTime;
+ private int endTime;
+ 
+ private Date startDate;        //렌트날짜 
+ private Date endDate;          //반납날짜
  private String cancel;           //취소여부
  private int amount;
  private String del;              //목록 삭제 
