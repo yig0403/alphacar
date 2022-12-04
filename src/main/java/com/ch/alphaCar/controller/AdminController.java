13ad3a4cfb59b59f529412e4beac0a0b4b1f17f1
yig <yig0403@naver.com>
2022-12-04 20:03:32 +0900

    진짜 최종

--- src/main/java/com/ch/alphaCar/controller/AdminController.java
+++ src/main/java/com/ch/alphaCar/controller/AdminController.java
@@ -1,10 +1,14 @@
 package com.ch.alphaCar.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ch.alphaCar.dto.Member;
 import com.ch.alphaCar.service.MemberService;
+import com.ch.alphaCar.service.NoticeService;
 import com.ch.alphaCar.service.WaysService;
 
 @Controller
@@ -15,13 +19,16 @@ public class AdminController {
 	private WaysService ws;
 	
 	@RequestMapping("adminMain.do")
-	public String adminMain() {
-		return "/admin/adminMain";
+	public String adminMain(HttpSession session) {		
+		String id = (String)session.getAttribute("id");		
+		if (id == null || !id.equals("admin")) {
+			return "/member/main2";
+		} else return "/admin/adminMain";
 	}
 	
 	@RequestMapping("serviceCenter.do")
 	public String serviceCenter() {
 		return "/sc/serviceCenter";
 	}
-	
+
 }
